b22d77c6124805e3620e01bf1c39c24496f34be5
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-05-31 12:51:42 +0300

    Add "getMyName" method

--- src/Method/GetMyName.php
+++ src/Method/GetMyName.php
@@ -6,38 +6,37 @@ namespace Vjik\TelegramBot\Api\Method;
 
 use Vjik\TelegramBot\Api\Request\HttpMethod;
 use Vjik\TelegramBot\Api\Request\TelegramRequestWithResultPreparingInterface;
+use Vjik\TelegramBot\Api\Type\BotName;
 
 /**
- * @see https://core.telegram.org/bots/api#setmyname
+ * @see https://core.telegram.org/bots/api#getmyname
  */
-final readonly class SetMyName implements TelegramRequestWithResultPreparingInterface
+final readonly class GetMyName implements TelegramRequestWithResultPreparingInterface
 {
     public function __construct(
-        private ?string $name = null,
         private ?string $languageCode = null,
     ) {
     }
 
     public function getHttpMethod(): HttpMethod
     {
-        return HttpMethod::POST;
+        return HttpMethod::GET;
     }
 
     public function getApiMethod(): string
     {
-        return 'setMyName';
+        return 'getMyName';
     }
 
     public function getData(): array
     {
         return array_filter([
-            'name' => $this->name,
             'language_code' => $this->languageCode,
         ]);
     }
 
-    public function prepareResult(mixed $result): true
+    public function prepareResult(mixed $result): mixed
     {
-        return true;
+        return BotName::fromTelegramResult($result);
     }
 }
---
+++ src/Type/BotName.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vjik\TelegramBot\Api\Type;
+
+use Vjik\TelegramBot\Api\ParseResult\ValueHelper;
+
+/**
+ * @see https://core.telegram.org/bots/api#botname
+ */
+final readonly class BotName
+{
+    public function __construct(
+        public string $name,
+    ) {
+    }
+
+    public static function fromTelegramResult(array $result): self
+    {
+        ValueHelper::assertArrayResult($result);
+        return new self(
+            ValueHelper::getString($result, 'name'),
+        );
+    }
+}
