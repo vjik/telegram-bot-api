7299170dcb7bcad60be11cddff31d33027d14dee
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-06-04 18:24:00 +0300

    Types 6

---
+++ tests/Type/Sticker/MaskPositionTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Type\Sticker;
+
+use PHPUnit\Framework\TestCase;
+use Vjik\TelegramBot\Api\Type\Sticker\MaskPosition;
+
+final class MaskPositionTest extends TestCase
+{
+    public function testMaskPosition(): void
+    {
+        $maskPosition = new MaskPosition(
+            'forehead',
+            0.5,
+            0.6,
+            0.7
+        );
+
+        $this->assertSame('forehead', $maskPosition->point);
+        $this->assertSame(0.5, $maskPosition->xShift);
+        $this->assertSame(0.6, $maskPosition->yShift);
+        $this->assertSame(0.7, $maskPosition->scale);
+    }
+
+    public function testFromTelegramResult(): void
+    {
+        $maskPosition = MaskPosition::fromTelegramResult([
+            'point' => 'forehead',
+            'x_shift' => 0.5,
+            'y_shift' => 0.6,
+            'scale' => 0.7,
+        ]);
+
+        $this->assertSame('forehead', $maskPosition->point);
+        $this->assertSame(0.5, $maskPosition->xShift);
+        $this->assertSame(0.6, $maskPosition->yShift);
+        $this->assertSame(0.7, $maskPosition->scale);
+    }
+}
