0c3c9a3a5e4e77fd21609c043a5ed2803bb2d9eb
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-06-05 15:58:56 +0300

    Method 2

---
+++ tests/Method/SendLocationTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vjik\TelegramBot\Api\Tests\Method;
+
+use PHPUnit\Framework\TestCase;
+use Vjik\TelegramBot\Api\Method\SendLocation;
+use Vjik\TelegramBot\Api\Request\HttpMethod;
+use Vjik\TelegramBot\Api\Type\ForceReply;
+use Vjik\TelegramBot\Api\Type\ReplyParameters;
+
+final class SendLocationTest extends TestCase
+{
+    public function testBase(): void
+    {
+        $method = new SendLocation(12, 1.1, 2.2);
+
+        $this->assertSame(HttpMethod::POST, $method->getHttpMethod());
+        $this->assertSame('sendLocation', $method->getApiMethod());
+        $this->assertSame(
+            [
+                'chat_id' => 12,
+                'latitude' => 1.1,
+                'longitude' => 2.2,
+            ],
+            $method->getData(),
+        );
+    }
+
+    public function testFull(): void
+    {
+        $replyParameters = new ReplyParameters(23);
+        $replyMarkup = new ForceReply();
+        $method = new SendLocation(
+            12,
+            1.1,
+            2.2,
+            'test',
+            99,
+            2.23,
+            5,
+            123,
+            1,
+            true,
+            false,
+            'id1',
+            $replyParameters,
+            $replyMarkup
+        );
+
+        $this->assertSame(
+            [
+                'business_connection_id' => 'test',
+                'chat_id' => 12,
+                'message_thread_id' => 99,
+                'latitude' => 1.1,
+                'longitude' => 2.2,
+                'horizontal_accuracy' => 2.23,
+                'live_period' => 5,
+                'heading' => 123,
+                'proximity_alert_radius' => 1,
+                'disable_notification' => true,
+                'protect_content' => false,
+                'message_effect_id' => 'id1',
+                'reply_parameters' => $replyParameters->toRequestArray(),
+                'reply_markup' => $replyMarkup->toRequestArray(),
+            ],
+            $method->getData()
+        );
+    }
+
+    public function testPrepareResult(): void
+    {
+        $method = new SendLocation(12, 1.1, 2.2);
+
+        $preparedResult = $method->prepareResult([
+            'message_id' => 7,
+            'date' => 1620000000,
+            'chat' => [
+                'id' => 1,
+                'type' => 'private',
+            ],
+        ]);
+
+        $this->assertSame(7, $preparedResult->messageId);
+    }
+}
---
+++ tests/Method/SendMessageTest.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vjik\TelegramBot\Api\Tests\Method;
+
+use PHPUnit\Framework\TestCase;
+use Vjik\TelegramBot\Api\Method\SendMessage;
+use Vjik\TelegramBot\Api\Request\HttpMethod;
+use Vjik\TelegramBot\Api\Type\ForceReply;
+use Vjik\TelegramBot\Api\Type\LinkPreviewOptions;
+use Vjik\TelegramBot\Api\Type\MessageEntity;
+use Vjik\TelegramBot\Api\Type\ReplyParameters;
+
+final class SendMessageTest extends TestCase
+{
+    public function testBase(): void
+    {
+        $method = new SendMessage(12, 'hello');
+
+        $this->assertSame(HttpMethod::POST, $method->getHttpMethod());
+        $this->assertSame('sendMessage', $method->getApiMethod());
+        $this->assertSame(
+            [
+                'chat_id' => 12,
+                'text' => 'hello',
+            ],
+            $method->getData(),
+        );
+    }
+
+    public function testFull(): void
+    {
+        $entity = new MessageEntity('bold', 0, 5);
+        $linkPreviewOptions = new LinkPreviewOptions(true);
+        $replyParameters = new ReplyParameters(23);
+        $replyMarkup = new ForceReply();
+        $method = new SendMessage(
+            12,
+            'hello',
+            'bcid1',
+            99,
+            'parse',
+            [$entity],
+            $linkPreviewOptions,
+            true,
+            false,
+            'meid1',
+            $replyParameters,
+            $replyMarkup,
+        );
+
+        $this->assertSame(
+            [
+                'business_connection_id' => 'bcid1',
+                'chat_id' => 12,
+                'message_thread_id' => 99,
+                'text' => 'hello',
+                'parse_mode' => 'parse',
+                'entities' => [$entity->toRequestArray()],
+                'link_preview_options' => $linkPreviewOptions->toRequestArray(),
+                'disable_notification' => true,
+                'protect_content' => false,
+                'message_effect_id' => 'meid1',
+                'reply_parameters' => $replyParameters->toRequestArray(),
+                'reply_markup' => $replyMarkup->toRequestArray(),
+            ],
+            $method->getData()
+        );
+    }
+
+    public function testPrepareResult(): void
+    {
+        $method = new SendMessage(12, 'hello');
+
+        $preparedResult = $method->prepareResult([
+            'message_id' => 7,
+            'date' => 1620000000,
+            'chat' => [
+                'id' => 1,
+                'type' => 'private',
+            ],
+        ]);
+
+        $this->assertSame(7, $preparedResult->messageId);
+    }
+}
