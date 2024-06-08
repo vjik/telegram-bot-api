2b62cca556ec7815592805b2415fa4ae84c760ea
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-06-08 09:05:02 +0300

    Add `SendVenue` method

---
+++ src/Method/SendVenue.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vjik\TelegramBot\Api\Method;
+
+use Vjik\TelegramBot\Api\Request\HttpMethod;
+use Vjik\TelegramBot\Api\Request\TelegramRequestWithResultPreparingInterface;
+use Vjik\TelegramBot\Api\Type\ForceReply;
+use Vjik\TelegramBot\Api\Type\InlineKeyboardMarkup;
+use Vjik\TelegramBot\Api\Type\Message;
+use Vjik\TelegramBot\Api\Type\ReplyKeyboardMarkup;
+use Vjik\TelegramBot\Api\Type\ReplyKeyboardRemove;
+use Vjik\TelegramBot\Api\Type\ReplyParameters;
+
+/**
+ * @see https://core.telegram.org/bots/api#sendvenue
+ */
+final class SendVenue implements TelegramRequestWithResultPreparingInterface
+{
+    public function __construct(
+        private int|string $chatId,
+        private float $latitude,
+        private float $longitude,
+        private string $title,
+        private string $address,
+        private ?string $businessConnectionId = null,
+        private ?int $messageThreadId = null,
+        private ?string $foursquareId = null,
+        private ?string $foursquareType = null,
+        private ?string $googlePlaceId = null,
+        private ?string $googlePlaceType = null,
+        private ?bool $disableNotification = null,
+        private ?bool $protectContent = null,
+        private ?string $messageEffectId = null,
+        private ?ReplyParameters $replyParameters = null,
+        private InlineKeyboardMarkup|ReplyKeyboardMarkup|ReplyKeyboardRemove|ForceReply|null $replyMarkup = null,
+    ) {
+    }
+
+    public function getHttpMethod(): HttpMethod
+    {
+        return HttpMethod::POST;
+    }
+
+    public function getApiMethod(): string
+    {
+        return 'sendVenue';
+    }
+
+    public function getData(): array
+    {
+        return array_filter(
+            [
+                'business_connection_id' => $this->businessConnectionId,
+                'chat_id' => $this->chatId,
+                'message_thread_id' => $this->messageThreadId,
+                'latitude' => $this->latitude,
+                'longitude' => $this->longitude,
+                'title' => $this->title,
+                'address' => $this->address,
+                'foursquare_id' => $this->foursquareId,
+                'foursquare_type' => $this->foursquareType,
+                'google_place_id' => $this->googlePlaceId,
+                'google_place_type' => $this->googlePlaceType,
+                'disable_notification' => $this->disableNotification,
+                'protect_content' => $this->protectContent,
+                'message_effect_id' => $this->messageEffectId,
+                'reply_parameters' => $this->replyParameters?->toRequestArray(),
+                'reply_markup' => $this->replyMarkup?->toRequestArray(),
+            ],
+            static fn(mixed $value): bool => $value !== null,
+        );
+    }
+
+    public function prepareResult(mixed $result): Message
+    {
+        return Message::fromTelegramResult($result);
+    }
+}
---
+++ tests/Method/SendVenueTest.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Method;
+
+use PHPUnit\Framework\TestCase;
+use Vjik\TelegramBot\Api\Method\SendVenue;
+use Vjik\TelegramBot\Api\Request\HttpMethod;
+use Vjik\TelegramBot\Api\Type\ForceReply;
+use Vjik\TelegramBot\Api\Type\ReplyParameters;
+
+final class SendVenueTest extends TestCase
+{
+    public function testBase(): void
+    {
+        $method = new SendVenue(12, 1.1, 2.2, 'Moscow', 'Kremlin');
+
+        $this->assertSame(HttpMethod::POST, $method->getHttpMethod());
+        $this->assertSame('sendVenue', $method->getApiMethod());
+        $this->assertSame(
+            [
+                'chat_id' => 12,
+                'latitude' => 1.1,
+                'longitude' => 2.2,
+                'title' => 'Moscow',
+                'address' => 'Kremlin',
+            ],
+            $method->getData(),
+        );
+    }
+
+    public function testFull(): void
+    {
+        $replyParameters = new ReplyParameters(23);
+        $replyMarkup = new ForceReply();
+        $method = new SendVenue(
+            12,
+            1.1,
+            2.2,
+            'Moscow',
+            'Kremlin',
+            'bcid1',
+            99,
+            'fs1',
+            'ft2',
+            'gp1',
+            'gt2',
+            true,
+            false,
+            'meid1',
+            $replyParameters,
+            $replyMarkup
+        );
+
+        $this->assertSame(
+            [
+                'business_connection_id' => 'bcid1',
+                'chat_id' => 12,
+                'message_thread_id' => 99,
+                'latitude' => 1.1,
+                'longitude' => 2.2,
+                'title' => 'Moscow',
+                'address' => 'Kremlin',
+                'foursquare_id' => 'fs1',
+                'foursquare_type' => 'ft2',
+                'google_place_id' => 'gp1',
+                'google_place_type' => 'gt2',
+                'disable_notification' => true,
+                'protect_content' => false,
+                'message_effect_id' => 'meid1',
+                'reply_parameters' => $replyParameters->toRequestArray(),
+                'reply_markup' => $replyMarkup->toRequestArray(),
+            ],
+            $method->getData(),
+        );
+    }
+
+    public function testPrepareResult(): void
+    {
+        $method = new SendVenue(12, 1.1, 2.2, 'Moscow', 'Kremlin');
+
+        $preparedResult = $method->prepareResult([
+            'message_id' => 7,
+            'date' => 1620000000,
+            'chat' => [
+                'id' => 1,
+                'type' => 'private',
+            ],
+        ]);
+
+        $this->assertSame(7, $preparedResult->messageId);
+    }
+}
