7f69eaa33ffc646aa43cc293a4da5d78d667f3e3
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-06-24 10:15:50 +0300

    Add `sendVoice` method (#28)

---
+++ src/Method/SendVoice.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vjik\TelegramBot\Api\Method;
+
+use Vjik\TelegramBot\Api\Request\HttpMethod;
+use Vjik\TelegramBot\Api\Request\TelegramRequestWithFilesInterface;
+use Vjik\TelegramBot\Api\Request\TelegramRequestWithResultPreparingInterface;
+use Vjik\TelegramBot\Api\Type\ForceReply;
+use Vjik\TelegramBot\Api\Type\InlineKeyboardMarkup;
+use Vjik\TelegramBot\Api\Type\InputFile;
+use Vjik\TelegramBot\Api\Type\Message;
+use Vjik\TelegramBot\Api\Type\MessageEntity;
+use Vjik\TelegramBot\Api\Type\ReplyKeyboardMarkup;
+use Vjik\TelegramBot\Api\Type\ReplyKeyboardRemove;
+use Vjik\TelegramBot\Api\Type\ReplyParameters;
+
+/**
+ * @see https://core.telegram.org/bots/api#sendvoice
+ */
+final readonly class SendVoice implements TelegramRequestWithResultPreparingInterface, TelegramRequestWithFilesInterface
+{
+    /**
+     * @param MessageEntity[]|null $captionEntities
+     */
+    public function __construct(
+        private int|string $chatId,
+        private string|InputFile $voice,
+        private ?string $businessConnectionId = null,
+        private ?int $messageThreadId = null,
+        private ?string $caption = null,
+        private ?string $parseMode = null,
+        private ?array $captionEntities = null,
+        private ?int $duration = null,
+        private ?bool $disableNotification = null,
+        private ?bool $protectContent = null,
+        private ?string $messageEffectId = null,
+        private ?ReplyParameters $replyParameters = null,
+        private InlineKeyboardMarkup|ReplyKeyboardMarkup|ReplyKeyboardRemove|ForceReply|null $replyMarkup = null,
+    ) {
+    }
+
+    public function getHttpMethod(): HttpMethod
+    {
+        return HttpMethod::POST;
+    }
+
+    public function getApiMethod(): string
+    {
+        return 'sendVoice';
+    }
+
+    public function getData(): array
+    {
+        return array_filter(
+            [
+                'business_connection_id' => $this->businessConnectionId,
+                'chat_id' => $this->chatId,
+                'message_thread_id' => $this->messageThreadId,
+                'voice' => is_string($this->voice) ? $this->voice : null,
+                'caption' => $this->caption,
+                'parse_mode' => $this->parseMode,
+                'caption_entities' => $this->captionEntities === null ? null : array_map(
+                    static fn(MessageEntity $entity) => $entity->toRequestArray(),
+                    $this->captionEntities,
+                ),
+                'duration' => $this->duration,
+                'disable_notification' => $this->disableNotification,
+                'protect_content' => $this->protectContent,
+                'message_effect_id' => $this->messageEffectId,
+                'reply_parameters' => $this->replyParameters?->toRequestArray(),
+                'reply_markup' => $this->replyMarkup?->toRequestArray(),
+            ],
+            static fn(mixed $value): bool => $value !== null,
+        );
+    }
+
+    public function getFiles(): array
+    {
+        return array_filter(
+            [
+                'voice' => $this->voice instanceof InputFile ? $this->voice : null,
+            ],
+            static fn(mixed $value): bool => $value !== null,
+        );
+    }
+
+    public function prepareResult(mixed $result): Message
+    {
+        return Message::fromTelegramResult($result);
+    }
+}
