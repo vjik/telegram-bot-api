742be402ef0b0f39c2baac093ed87a7ca66af693
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-06-25 09:52:48 +0300

    Add `banChatMember` and `unbanChatMember` methods

---
+++ tests/Method/BanChatMemberTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vjik\TelegramBot\Api\Tests\Method;
+
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+use Vjik\TelegramBot\Api\Method\BanChatMember;
+use Vjik\TelegramBot\Api\Request\HttpMethod;
+
+final class BanChatMemberTest extends TestCase
+{
+    public function testBase(): void
+    {
+        $method = new BanChatMember(1, 2);
+
+        $this->assertSame(HttpMethod::POST, $method->getHttpMethod());
+        $this->assertSame('banChatMember', $method->getApiMethod());
+        $this->assertSame(
+            [
+                'chat_id' => 1,
+                'user_id' => 2,
+            ],
+            $method->getData(),
+        );
+    }
+
+    public function testFull(): void
+    {
+        $date = new DateTimeImmutable();
+        $method = new BanChatMember(1, 2, $date, true);
+
+        $this->assertSame(
+            [
+                'chat_id' => 1,
+                'user_id' => 2,
+                'until_date' => $date->getTimestamp(),
+                'revoke_messages' => true,
+            ],
+            $method->getData()
+        );
+    }
+
+    public function testPrepareResult(): void
+    {
+        $method = new BanChatMember(1, 2);
+
+        $preparedResult = $method->prepareResult(true);
+
+        $this->assertTrue($preparedResult);
+    }
+}
