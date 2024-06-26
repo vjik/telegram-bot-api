80e1278fbbc1105e992be43342715fdd5fde7616
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-06-26 21:51:14 +0300

    Add `approveChatJoinRequest` and `declineChatJoinRequest` methods

---
+++ src/Method/DeclineChatJoinRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vjik\TelegramBot\Api\Method;
+
+use Vjik\TelegramBot\Api\Request\HttpMethod;
+use Vjik\TelegramBot\Api\Request\TelegramRequestWithResultPreparingInterface;
+
+/**
+ * @see https://core.telegram.org/bots/api#declinechatjoinrequest
+ */
+final readonly class DeclineChatJoinRequest implements TelegramRequestWithResultPreparingInterface
+{
+    public function __construct(
+        private int|string $chatId,
+        private int $userId,
+    ) {
+    }
+
+    public function getHttpMethod(): HttpMethod
+    {
+        return HttpMethod::POST;
+    }
+
+    public function getApiMethod(): string
+    {
+        return 'declineChatJoinRequest';
+    }
+
+    public function getData(): array
+    {
+        return [
+            'chat_id' => $this->chatId,
+            'user_id' => $this->userId,
+        ];
+    }
+
+    public function prepareResult(mixed $result): true
+    {
+        return true;
+    }
+}
