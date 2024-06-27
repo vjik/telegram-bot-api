177b52a9a6e9a97cf679d6034c7a8bd15767feb2
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-06-27 13:30:42 +0300

    cleanup

--- src/Type/Sticker/Sticker.php
+++ src/Type/Sticker/Sticker.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Vjik\TelegramBot\Api\Type\Sticker;
 
-use Vjik\TelegramBot\Api\Constant\Sticker\StickerType;
 use Vjik\TelegramBot\Api\ParseResult\ValueHelper;
 use Vjik\TelegramBot\Api\Type\File;
 use Vjik\TelegramBot\Api\Type\PhotoSize;
