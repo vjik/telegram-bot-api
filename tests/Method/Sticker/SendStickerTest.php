e09ee7b0364aad567f5f8e6720f7c2cfed7db87a
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-06-27 20:56:53 +0300

    Add `sendSticker` method (#52)

---
+++ tests/Method/Sticker/SendStickerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vjik\TelegramBot\Api\Tests\Method\Sticker;
+
+use HttpSoft\Message\StreamFactory;
+use PHPUnit\Framework\TestCase;
+use Vjik\TelegramBot\Api\Method\Sticker\SendSticker;
+use Vjik\TelegramBot\Api\Request\HttpMethod;
+use Vjik\TelegramBot\Api\Type\ForceReply;
+use Vjik\TelegramBot\Api\Type\InputFile;
+use Vjik\TelegramBot\Api\Type\ReplyParameters;
+
+final class SendStickerTest extends TestCase
+{
+    public function testBase(): void
+    {
+        $method = new SendSticker(12, 'https://example.com/sticker.webp');
+
+        $this->assertSame(HttpMethod::POST, $method->getHttpMethod());
+        $this->assertSame('sendSticker', $method->getApiMethod());
+        $this->assertSame(
+            [
+                'chat_id' => 12,
+                'sticker' => 'https://example.com/sticker.webp',
+            ],
+            $method->getData(),
+        );
+    }
+
+    public function testFull(): void
+    {
+        $sticker = new InputFile((new StreamFactory())->createStream());
+        $replyParameters = new ReplyParameters(23);
+        $replyMarkup = new ForceReply();
+        $method = new SendSticker(
+            12,
+            $sticker,
+            'bcid1',
+            99,
+            '👍',
+            true,
+            false,
+            'meid1',
+            $replyParameters,
+            $replyMarkup,
+        );
+
+        $this->assertSame(
+            [
+                'business_connection_id' => 'bcid1',
+                'chat_id' => 12,
+                'message_thread_id' => 99,
+                'sticker' => $sticker,
+                'emoji' => '👍',
+                'disable_notification' => true,
+                'protect_content' => false,
+                'message_effect_id' => 'meid1',
+                'reply_parameters' => $replyParameters->toRequestArray(),
+                'reply_markup' => $replyMarkup->toRequestArray(),
+            ],
+            $method->getData(),
+        );
+    }
+
+    public function testPrepareResult(): void
+    {
+        $method = new SendSticker(12, 'https://example.com/sticker.webp');
+
+        $preparedResult = $method->prepareResult([
+            'message_id' => 7,
+            'date' => 1620000000,
+            'chat' => [
+                'id' => 1,
+                'type' => 'private',
+            ],
+        ]);
+
+        $this->assertSame(7, $preparedResult->messageId);
+    }
+}
--- tests/TelegramBotApiTest.php
+++ tests/TelegramBotApiTest.php
@@ -947,6 +947,26 @@ final class TelegramBotApiTest extends TestCase
         $this->assertSame(7, $result->messageId);
     }
 
+    public function testSendSticker(): void
+    {
+        $api = $this->createApi([
+            'ok' => true,
+            'result' => [
+                'message_id' => 7,
+                'date' => 1620000000,
+                'chat' => [
+                    'id' => 1,
+                    'type' => 'private',
+                ],
+            ],
+        ]);
+
+        $result = $api->sendSticker(12, 'https://example.com/sticker.webp');
+
+        $this->assertInstanceOf(Message::class, $result);
+        $this->assertSame(7, $result->messageId);
+    }
+
     public function testSendVenue(): void
     {
         $api = $this->createApi([
