510c49112463b7a6faff25f4e22a0aa829ec92e9
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-06-28 13:34:48 +0300

    Add `ForumTopicIconColor `

---
+++ src/Constant/ForumTopicIconColor.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vjik\TelegramBot\Api\Constant;
+
+final class ForumTopicIconColor
+{
+    const BLUE = 0x6FB9F0;
+    const ORANGE = 0xFFD67E;
+    const MAGENTA = 0xCB86DB;
+    const GREEN = 0x8EEE98;
+    const PINK = 0xFF93B2;
+    const RED = 0xFB6F5F;
+}
