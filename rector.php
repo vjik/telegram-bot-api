4c5c2f10042b3f1ef842a99166e2bb8e8aabbfa9
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-02-15 18:34:59 +0300

    add rector

---
+++ rector.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpSets(php82: true)
+    ->withSkip([
+        ClosureToArrowFunctionRector::class,
+        NullToStrictStringFuncCallArgRector::class,
+        ClassPropertyAssignToConstructorPromotionRector::class,
+    ]);
