5ea0cf20458826461c60b101f16c747fcf5589f5
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-10-15 10:09:53 +0300

    improve

--- src/WebhookResponse/MethodNotSupportedException.php
+++ src/WebhookResponse/MethodNotSupportedException.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Vjik\TelegramBot\Api\WebhookResponse;
 
-use LogicException;
+use InvalidArgumentException;
 
 /**
  * Exception thrown when a method doesn't support sending via a webhook response.
  *
  * @api
  */
-final class MethodNotSupportedException extends LogicException {}
+final class MethodNotSupportedException extends InvalidArgumentException {}
